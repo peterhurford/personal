8c7343ba27a87e7a2f5561fe1d57f1df30ca47fc
Peter Hurford <peter@peterhurford.com>
2014-03-21 02:16:05 -0400

    Added note to dock.js

--- assets/js/dock.js
+++ assets/js/dock.js
@@ -1,3 +1,11 @@
+/* 
+Previous selector helper function for the "mac dock" style footer
+
+CSS can select the next element, but cannot automatically select the previous element, so we close the gap here with JS.
+
+Adapted from GirlieMac <http://girliemac.com/blog/2010/06/02/simulating-macos-dock-like-menu-with-css3/>.
+*/
+
 function addPrevClass (e) {
   var target = e.target;
     if(target.getAttribute('src')) { // check if it is img
