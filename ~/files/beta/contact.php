a98d14e98904634bbb1959c1ce8f263e0a474dae
Peter Hurford <peter@peterhurford.com>
2014-05-07 21:56:38 -0400

    Removed unneeded stored files

--- ~/files/beta/contact.php
+++
@@ -1,9 +0,0 @@
-<?php include("header.php"); ?>
-<script>
-	var defaultimg = "pics/contact.jpg";
-</script>
-<div id="content">
-	<h1>Contact</h1>
-	<p>Coming soon!</p>
-</div>
-<?php include("footer.php"); ?>
--- ~/files/beta/schedule.php
+++
@@ -1,9 +0,0 @@
-<?php include("header.php"); ?>
-<script>
-	var defaultimg = "pics/schedule.jpg";
-</script>
-<div id="content">
-	<h1>Event Schedule</h1>
-	<p>Coming soon!</p>
-</div>
-<?php include("footer.php"); ?>
